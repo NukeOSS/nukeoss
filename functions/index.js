7e4a71c7ec9371067cef28d391a3e59909beb6a6
vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>
2020-05-28 03:03:11 +0530

    feat(webapi): Added Nodejs Based Webapi
    Signed-off-by: vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,42 +8,8 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-"use strict";
-
-//import * as admin from 'firebase-admin';
-const express = require("express");
-var cors = require('cors')
-const bodyParser = require("body-parser");
-const sessid = require(".//utils");
-
-var admin = require("firebase-admin");
-
-
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
- databaseURL: "http://localhost:5001"
-});
-
-const db = admin.firestore();
-const app = express();
-exports.app = app;
-const main = express();
-main.use(bodyParser.json());
-main.use(bodyParser.urlencoded({ extended: true }));
-main.use('/api/v1', app);
-exports.webApi = functions.https.onRequest(main);
-app.use(cors())
-
-// Add new prospect  
-app.post('/createsession', async (req, res) => {
-    const sessionId = sessid.createSessionID();
-
-    const sessionReturn={
-        session:sessionId
-    };
-    res.status(200).send(sessionReturn); 
-});
-
-app.get('/createsession', async (req, res) => {
-   res.status(400).send('Not Supported'); 
+exports.createsession = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        response.send("Hello from Firebase!");
+    });
 });
--- public/script/firebaseScript.js
+++ public/script/firebaseScript.js
@@ -19,52 +19,52 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 var firebaseConfig = {
     // Please Enter Your Firebase Configuration Here.
     apiKey: "AIzaSyCilDU92wZHSgov84nCsjHvHkBaTUEi1q8",
-  authDomain: "scrumpoker-3a0ac.firebaseapp.com",
-  databaseURL: "https://scrumpoker-3a0ac.firebaseio.com",
-  projectId: "scrumpoker-3a0ac",
-  storageBucket: "scrumpoker-3a0ac.appspot.com",
-  messagingSenderId: "254261994841",
-  appId: "1:254261994841:web:a78081d97e0d5550465848",
-  measurementId: "G-XK3Y52BFJ0"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+    authDomain: "scrumpoker-3a0ac.firebaseapp.com",
+    databaseURL: "https://scrumpoker-3a0ac.firebaseio.com",
+    projectId: "scrumpoker-3a0ac",
+    storageBucket: "scrumpoker-3a0ac.appspot.com",
+    messagingSenderId: "254261994841",
+    appId: "1:254261994841:web:a78081d97e0d5550465848",
+    measurementId: "G-XK3Y52BFJ0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
-callbacks: {
-signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-// User successfully signed in.
-// Return type determines whether we continue the redirect automatically
-// or whether we leave that to developer to handle.
-return true;
-},
-uiShown: function() {
-// The widget is rendered.
-// Hide the loader.
-document.getElementById('loader').style.display = 'none';
-}
-},
-// Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-signInFlow: 'popup',
-signInSuccessUrl: 'index.html',
-signInOptions: [
-// Leave the lines as is for the providers you want to offer your users.
-{
-provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-scopes: [
-        'https://www.googleapis.com/auth/contacts.readonly'
-    ],
-},
-firebase.auth.EmailAuthProvider.PROVIDER_ID
-]
-// Terms of service url.
-// tosUrl: '<your-tos-url>',
-// // Privacy policy url.
-// privacyPolicyUrl: '<your-privacy-policy-url>'
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'index.html',
+    signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            {
+                provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                scopes: [
+                    'https://www.googleapis.com/auth/contacts.readonly'
+                ],
+            },
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ]
+        // Terms of service url.
+        // tosUrl: '<your-tos-url>',
+        // // Privacy policy url.
+        // privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
 // Initialize Cloud Firestore through Firebase
