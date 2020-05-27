aa49cd50515e521f98e8ce381329c3337b2378bb
sjitendra <jitendra@nanoheal.com>
2020-05-27 18:31:01 +0530

    Add session with base-62

---
+++ public/script/utils.js
@@ -0,0 +1,28 @@
+/* Generate the base-62 key*/
+const base62 = {
+  charset: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split(''),
+  encode: integer => {
+    if (integer === 0) {
+      return 0;
+    }
+    let s = [];
+    while (integer > 0) {
+      s = [base62.charset[integer % 62], ...s];
+      integer = Math.floor(integer / 62);
+    }
+    return s.join('');
+  },
+  decode: chars => chars.split('').reverse().reduce((prev, curr, i) =>
+    prev + (base62.charset.indexOf(curr) * (62 ** i)), 0)
+};
+
+/*Create base-62 based ID with mili second based time*/
+function createSessionID(){
+	
+	var dateLocal = new Date();
+	var miliSec = dateLocal.getTime();
+	returnSession = base62.encode(miliSec);
+	return returnSession;
+
+}
