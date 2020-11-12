d4d00ea8bb824b90ff7abf28d642c00d32cf2168
vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>
2020-11-12 17:14:51 +0530

    feat(update): Updated Packages
    Signed-off-by: vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>

--- nukeoss-ui/src/app/body/body.component.spec.ts
+++ nukeoss-ui/src/app/body/body.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BodyComponent } from './body.component';
 
@@ -6,7 +6,7 @@ describe('BodyComponent', () => {
   let component: BodyComponent;
   let fixture: ComponentFixture<BodyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BodyComponent ]
     })
--- nukeoss-ui/src/app/body/joinsession/joinsession.component.spec.ts
+++ nukeoss-ui/src/app/body/joinsession/joinsession.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { JoinsessionComponent } from './joinsession.component';
 
@@ -6,7 +6,7 @@ describe('JoinsessionComponent', () => {
   let component: JoinsessionComponent;
   let fixture: ComponentFixture<JoinsessionComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ JoinsessionComponent ]
     })
